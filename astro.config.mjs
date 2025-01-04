cf57ab8f9f3c30b7c172693be5e316a3a3792ec4
GasipiValentino <valentino.gasipi@davinci.edu.ar>
2025-01-04 19:43:29 -0300

    i18n Setup and Configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,16 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+    fallback: {
+      en: 'es',
+      es: 'en',
+    }
+  },
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,24 @@
+export const languages = {
+    es: 'Español',
+    en: 'English',
+  };
+  
+  export const defaultLang = 'en';
+  export const showDefaultLang = false;
+  
+  export const ui = {
+    es: {
+      'nav.home': 'Inicio',
+      'nav.experience': 'Experiencia',
+      'nav.projects': 'Proyectos',
+      'nav.about-me': 'Sobre Mi',
+      'nav.contact': 'Contacto',
+    },
+    en: {
+        'nav.home': 'Home',
+        'nav.experience': 'Experience',
+        'nav.projects': 'Projects',
+        'nav.about-me': 'About Me',
+        'nav.contact': 'Contact',
+    },
+  } as const;
