3815d2d97d2eb0efc86febd176102ccea6509e5a
GasipiValentino <valentino.gasipi@davinci.edu.ar>
2025-08-30 16:47:51 -0300

    Add Reemo project

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -13,35 +13,43 @@ export const languages = {
       'nav.projects': 'Proyectos',
       'nav.about-me': 'Sobre Mi',
       'nav.contact': 'Contacto',
+
       'footer.about-me': 'Sobre Mi',
       'footer.contact': 'Contacto',
+
       'experience.title': 'Mancruz - Dogos de Burdeos',
       'experience.label': 'Última experiencia',
       'experience.date': 'Febrero 2024',
       'experience.description': 'Esta es mi primera y única experiencia laboral hasta el momento. Es un sitio web para un criador de perros. Es un sitio web simple con mucha información sobre la raza y los perros. Es un sitio web estático construido con HTML, CSS, algo de JS y Bootstrap.',
       'experience.button': 'Ver proyecto',
+
       'project.old-money.title': 'Old Money - Proyecto de Laravel',
       'project.old-money.description': 'Este proyecto es una plataforma de comercio electrónico construida con Laravel y Tailwind CSS. Utiliza Eloquent ORM para la gestión de la base de datos. Tiene un carrito de compras, pago, y panel de administración, en el que puedes gestionar productos, pedidos y usuarios.',
+      'project.old-money.github': 'Código',
+      'project.old-money.link': 'Pronto...',
+
       'project.de-pelicula.title': 'De Película - Proyecto de Vue',
       'project.de-pelicula.description': 'Este proyecto usa el framework Vue y Vuetify para la UI. Consume la api TMDB para mostrar las últimas películas y series de televisión. Tiene una barra de búsqueda y una página de detalles para cada película. También puedes añadirlas a favoritos y filtrar por popularidad.',
+      'project.de-pelicula.github': 'Código',
+      'project.de-pelicula.link': 'Ver proyecto',
+
       'project.js-cart.title': 'JS Carrito - Proyecto de JavaScript',
       'project.js-cart.description': 'Este proyecto busca modelar y entender cómo funciona un carrito de compras. Se creó un e-commerce de zapatillas utilizando solo JavaScript Vanilla para el manejo de eventos del DOM. La página se caracteriza por su carga dinámica.',
+      'project.js-cart.github': 'Código',
+      'project.js-cart.link': 'Ver proyecto',
+      
       'project.statify.title': 'Statify - Spotify API',
       'project.statify.description': 'Esta aplicación te da la posibilidad de ver tus artistas, canciones y géneros más escuchados en diferentes períodos de tiempo. Descubre tendencias en tu historial de escucha y compara tus estadísticas con las de tus amigos.',
+      'project.statify.github': 'Código',
+      'project.statify.link': 'Ver Proyecto',
 
       'project.vecinos.title': 'Vecinos Conectados - Diseño UI/UX',
       'project.vecinos.description': 'Aplicación móvil diseñada en Figma para mejorar la convivencia vecinal. Incluye diferentes funcionalidades como enviar alertas a un vecino, reservas amenities, entre otras. Para la misma se desarrolló la marca completa (moodboards, UI Kit, userflows).',
-
-      'project.statify.github': 'Código',
-      'project.statify.link': 'Ver Proyecto',
-      'project.old-money.github': 'Código',
-      'project.old-money.link': 'Pronto...',
-      'project.de-pelicula.github': 'Código',
-      'project.de-pelicula.link': 'Ver proyecto',
-      'project.js-cart.github': 'Código',
-      'project.js-cart.link': 'Ver proyecto',
-
       'project.vecinos.link': 'Ver prototipo',
+
+      'project.reemo.title': 'Reemo - Plataforma de alquiler de vehículos',
+      'project.reemo.description': 'Aplicación web desarrollada como proyecto personal que permite a los usuarios alquilar vehículos de manera rápida y segura. La plataforma incluye funciones como registro e inicio de sesión de usuarios, gestión de autos disponibles, solicitudes de alquiler y seguimiento de vehículos arrendados.',
+      'project.reemo.link': 'Ver Proyecto',
 
       'p1': 'Mi nombre es Valentino. ',
       'strong1': 'Con un grado en Diseño y Programación Web,',
@@ -61,35 +69,45 @@ export const languages = {
         'nav.projects': 'Projects',
         'nav.about-me': 'About Me',
         'nav.contact': 'Contact',
+
         'footer.about-me': 'About Me',
         'footer.contact': 'Contact',
+
         'experience.title': 'Mancruz - Burdeos Dogos',
         'experience.label': 'Last experience',
         'experience.date': 'February 2024',
         'experience.description': 'This is my first and unique work experience at the moment. It is a website for a dog breeder. It is a simple website with a lot of information about the breed and the dogs. It is a static website built with HTML, CSS, some JS and Bootstrap.',
         'experience.button': 'View project',
+
         'project.old-money.title': 'Old Money - Laravel Project',
         'project.old-money.description': 'This project is an e-commerce platform built with Laravel and Tailwind CSS. It uses Eloquent ORM for database management. It has a shopping cart, payment, and administration panel, where you can manage products, orders, and users.',
+        'project.old-money.github': 'Code',
+        'project.old-money.link': 'Soon...',
+
         'project.de-pelicula.title': 'De Película - Vue Project',
         'project.de-pelicula.description': 'This project uses the Vue framework and Vuetify for the UI. It consumes the TMDB API to show the latest movies, and TV shows. It has a search bar, and a detail page for each movie. You can also add them to favorites and filter by popularity.',
+        'project.de-pelicula.github': 'Code',
+        'project.de-pelicula.link': 'View project',
+        
         'project.js-cart.title': 'JS Cart - JavaScript Project',
         'project.js-cart.description': 'This project aims to model and understand how a shopping cart works. A sneaker e-commerce was created using only Vanilla JavaScript for DOM event handling. The page is characterized by its dynamic loading.',
+        'project.js-cart.github': 'Code',
+        'project.js-cart.link': 'View project',
+        
         'project.statify.title': 'Statify - Spotify API',
         'project.statify.description': 'This app gives you the possibility to check out your most listened-to artists, tracks, and genres over different time periods. Discover trends in your listening history, and compare your stats with friends!',
         'project.statify.github': 'Code',
         'project.statify.link': 'View Project',
-        'project.old-money.github': 'Code',
-        'project.old-money.link': 'Soon...',
-        'project.de-pelicula.github': 'Code',
-        'project.de-pelicula.link': 'View project',
-        'project.js-cart.github': 'Code',
-        'project.js-cart.link': 'View project',
-
-        'project.vecinos.link': 'View prototype',
         
         'project.vecinos.title': 'Connected Neighbors - UI/UX Design',
         'project.vecinos.description': 'Figma-designed mobile app for neighborhood relations, featuring neighbor alerts, amenity bookings, and other functions. Included full brand development (moodboards, UI Kit, userflows).',
+        'project.vecinos.link': 'View prototype',
 
+        'project.reemo.title': 'Reemo – Vehicle Rental Platform',
+        'project.reemo.description': 'A web application developed as a personal project that allows users to rent vehicles quickly and safely. The platform includes features such as user registration and login, available car management, rental requests, and tracking of rented vehicles.',
+        // 'project.reemo.github': 'Code',
+        'project.reemo.link': 'View Project',
+        
         'p1': 'My name is Valentino.',
         'strong1': ' With a degree in Web Design and Programming,',
         'p2': " I've spent the past 2 years working on projects ranging from small business websites to interactive web applications. My journey began with a simple curiosity about how websites work, which grew into a career",
